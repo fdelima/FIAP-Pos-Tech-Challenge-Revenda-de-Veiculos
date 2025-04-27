3906c3de204670e707cca36fcb0226f933ec92c7
Fernando Lima <fernando@umavida.com>
2025-04-27 02:13:24 -0300

    Melhorias no log

--- Src/Api/GlobalErrorHandlingMiddleware.cs
+++ Src/Api/GlobalErrorHandlingMiddleware.cs
@@ -1,7 +1,11 @@
-﻿using FIAP.Pos.Tech.Challenge.RevendaDeVeiculos.Domain.Messages;
+﻿using Azure.Core;
+using FIAP.Pos.Tech.Challenge.RevendaDeVeiculos.Domain.Messages;
 using FIAP.Pos.Tech.Challenge.RevendaDeVeiculos.Domain.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Net;
+using System.Text;
 using System.Text.Json;
 
 namespace FIAP.Pos.Tech.Challenge.Api
@@ -33,18 +37,29 @@ namespace FIAP.Pos.Tech.Challenge.Api
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, ex.Message);
+                _logger.LogError(ex, string.Concat(ex.InnerException?.Message ?? ex.Message, context.Request.Method,
+                context.Request.IsHttps ? " https://" : " http://",
+                context.Request.Host.Value,
+                context.Request.Path.Value ?? "",
+                context.Request.QueryString.Value ?? ""));
                 await HandleExceptionAsync(context, ex);
             }
         }
 
-        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        private static Task HandleExceptionAsync(HttpContext context, Exception ex)
         {
-            string stackTrace = (exception.StackTrace ?? "");
+            string stackTrace = (ex.StackTrace ?? "");
             stackTrace = stackTrace.Length > 200 ? stackTrace.IndexOf(" in ") > -1 ? stackTrace.Substring(0, stackTrace.IndexOf(" in ")) : stackTrace.Substring(0, 200) : stackTrace;
             stackTrace += " ...";
 
-            var m = ModelResultFactory.Error(new { }, exception.Message, stackTrace);
+            var m = ModelResultFactory.Error<object>(default!,
+                string.Concat(context.Request.Method,
+                context.Request.IsHttps ? " https://" : " http://",
+                context.Request.Host.Value,
+                context.Request.Path.Value ?? "",
+                context.Request.QueryString.Value ?? ""),
+                ex.InnerException?.Message ?? ex.Message,
+                stackTrace);
             m.AddMessage(ErrorMessages.InternalServerError);
 
             string exceptionResult = JsonSerializer.Serialize(m);
