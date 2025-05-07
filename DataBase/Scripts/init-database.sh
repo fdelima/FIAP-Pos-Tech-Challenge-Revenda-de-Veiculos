c81503b2588efb42ea3e0a4f1aa18189e5f62128
Fernando Lima <fernando@umavida.com>
2025-05-07 08:48:57 -0300

    Melhoria logica das variaveis de ambiente

--- DataBase/Scripts/init-database.sh
+++ DataBase/Scripts/init-database.sh
@@ -5,12 +5,12 @@ sleep 5
 
 echo "Executando der"
 echo "Executando part 1"
-/opt/mssql-tools/bin/sqlcmd -S $instance -U $user -P $password -d master -i /tmp/tech-challenge-mer-1.sql
+/opt/mssql-tools/bin/sqlcmd -S $INSTANCE -U $USER -P $PASSWORD -d master -i /tmp/tech-challenge-mer-1.sql
 exit_code=$?
 if [ $exit_code -eq 0 ]; then
     echo "Executando part 2"
     sleep 5
-    /opt/mssql-tools/bin/sqlcmd -S $instance -U $user -P $password -d tech-challenge-revenda-de-veiculos -i /tmp/tech-challenge-mer-2.sql
+    /opt/mssql-tools/bin/sqlcmd -S $INSTANCE -U $USER -P $PASSWORD -d tech-challenge-revenda-de-veiculos -i /tmp/tech-challenge-mer-2.sql
     echo "DER executado :: Successfully " $exit_code
 else
 for (( i = 1; i <= 3; i++ ))
@@ -18,12 +18,12 @@ do
     sleep 5
     echo "Executando der :: Nova tentativa :: " $i
     echo "Executando part 1"
-    /opt/mssql-tools/bin/sqlcmd -S $instance -U $user -P $password -d master -i /tmp/tech-challenge-mer-1.sql
+    /opt/mssql-tools/bin/sqlcmd -S $INSTANCE -U $USER -P $PASSWORD -d master -i /tmp/tech-challenge-mer-1.sql
     exit_code=$?
     if [ $exit_code -eq 0 ]; then
         echo "Executando part 2"
         sleep 5
-        /opt/mssql-tools/bin/sqlcmd -S $instance -U $user -P $password -d tech-challenge-revenda-de-veiculos -i /tmp/tech-challenge-mer-2.sql
+        /opt/mssql-tools/bin/sqlcmd -S $INSTANCE -U $USER -P $PASSWORD -d tech-challenge-revenda-de-veiculos -i /tmp/tech-challenge-mer-2.sql
         echo "DER executado :: Successfully " $?
         break
     fi
