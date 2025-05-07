40327655685144fee07157e7ba87697208aec953
Fernando Lima <fernando@umavida.com>
2025-05-07 09:06:37 -0300

    MELHORIAS LOG E SCRIPT DE CRIAÇÃO DATABASE

--- DataBase/Scripts/init-database.sh
+++ DataBase/Scripts/init-database.sh
@@ -4,11 +4,11 @@ echo "Aguardando serviço sqlserver ficar pronto."
 sleep 5
 
 echo "Executando der"
-echo "Executando part 1"
+echo "Executando part 1 :: Criação do banco de dados"
 /opt/mssql-tools/bin/sqlcmd -S $INSTANCE -U $USER -P $PASSWORD -d master -i /tmp/tech-challenge-mer-1.sql
 exit_code=$?
 if [ $exit_code -eq 0 ]; then
-    echo "Executando part 2"
+    echo "Executando part 2 :: Criação da estrutura do banco de dados, tabelas, dados default, etc"
     sleep 5
     /opt/mssql-tools/bin/sqlcmd -S $INSTANCE -U $USER -P $PASSWORD -d tech-challenge-revenda-de-veiculos -i /tmp/tech-challenge-mer-2.sql
     echo "DER executado :: Successfully " $exit_code
@@ -17,11 +17,11 @@ for (( i = 1; i <= 3; i++ ))
 do 
     sleep 5
     echo "Executando der :: Nova tentativa :: " $i
-    echo "Executando part 1"
+    echo "Executando part 1 :: Criação do banco de dados"
     /opt/mssql-tools/bin/sqlcmd -S $INSTANCE -U $USER -P $PASSWORD -d master -i /tmp/tech-challenge-mer-1.sql
     exit_code=$?
     if [ $exit_code -eq 0 ]; then
-        echo "Executando part 2"
+        echo "Executando part 2 :: Criação da estrutura do banco de dados, tabelas, dados default, etc"
         sleep 5
         /opt/mssql-tools/bin/sqlcmd -S $INSTANCE -U $USER -P $PASSWORD -d tech-challenge-revenda-de-veiculos -i /tmp/tech-challenge-mer-2.sql
         echo "DER executado :: Successfully " $?
